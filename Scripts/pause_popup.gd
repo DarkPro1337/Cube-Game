b7661a9cc4675df537d74e57a9aa68c6c49bbc2b
DarkPro1337 <darkprothekun@gmail.com>
2019-07-29 00:48:34 +0300

    fixed shadows gap

--- Scripts/pause_popup.gd
+++ Scripts/pause_popup.gd
@@ -1,7 +1,9 @@
 extends Panel
 
+# var that changes if you press the pause
 var paused = false
 
+# input func that sets the var value and shows/hide pause popup with fancy blur *o*
 func _process(delta):
 	if Input.is_action_just_pressed("ui_pause") and !global.gamemanager.gameHasEnded:
 		if paused == false:
