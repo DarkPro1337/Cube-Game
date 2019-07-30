bfd88d8564cdea0cfbd59d1d4a5b1b1580410788
DarkPro1337 <darkprothekun@gmail.com>
2019-07-31 00:25:04 +0300

    menu selector position fixed
    now it will work correctly on resolution higher than default

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,14 +1,15 @@
 extends Control
 
-var button = {0: Vector2(404, 307), 1: Vector2(404, 361), 2: Vector2(404, 415)}
+var button = {0: Vector2(5, 4.883), 1: Vector2(5, 5.147), 2: Vector2(5, 5)}
 var selection = 0
 var in_settings = false
+var selected
 
 # enables fadeout
 func _ready():
 	global.menu = self
 	fadeOut_enable()
-	$Welcome/Selection.rect_position = button[0]
+	$Welcome/Selection.rect_position = $Welcome/Start.rect_position - Vector2(5, 4.883)
 
 func selection_check():
 	if selection < 0:
@@ -17,14 +18,15 @@ func selection_check():
 		selection = 2
 
 func _process(delta):
+	selected = {0: Vector2($Welcome/Start.rect_position), 1: Vector2($Welcome/Settings.rect_position), 2: Vector2($Welcome/Quit.rect_position)}
 	if Input.is_action_just_pressed("ui_up") and in_settings == false:
 		selection -= 1
 		selection_check()
-		$Welcome/Selection.rect_position = button[selection]
+		$Welcome/Selection.rect_position = selected[selection] - button[selection]
 	elif Input.is_action_just_pressed("ui_down") and in_settings == false:
 		selection += 1
 		selection_check()
-		$Welcome/Selection.rect_position = button[selection]
+		$Welcome/Selection.rect_position = selected[selection] - button[selection]
 	
 	if Input.is_action_just_pressed("ui_accept") and in_settings == false:
 		if selection == 0:
@@ -53,12 +55,12 @@ func _on_Quit_pressed():
 
 func _on_Start_mouse_entered():
 	selection = 0
-	$Welcome/Selection.rect_position = button[selection]
+	$Welcome/Selection.rect_position = selected[selection] - button[selection]
 
 func _on_Settings_mouse_entered():
 	selection = 1
-	$Welcome/Selection.rect_position = button[selection]
+	$Welcome/Selection.rect_position = selected[selection] - button[selection]
 
 func _on_Quit_mouse_entered():
 	selection = 2
-	$Welcome/Selection.rect_position = button[selection]
+	$Welcome/Selection.rect_position = selected[selection] - button[selection]
